d804bcefa3648966b0cadf116dbba79d21443925
Wojciech Twaróg <wojciech.twarog@avra.pl>
2018-08-10 12:37:29 +0200

    dodano background image

--- src/rootComponents/App/App.js
+++ src/rootComponents/App/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-            <h1 className="App-title">Welcome Table Football in Avra</h1>
+            <h1 className="App-title">Welcome to Table Football in Avra</h1>
         </header>
         <Home />
       </div>

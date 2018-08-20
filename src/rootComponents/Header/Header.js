ce72a8157370ae3c1a0c87eaa93451bdbcfc3a22
Wojciech Twaróg <wojciech.twarog@avra.pl>
2018-08-20 14:31:54 +0200

    added header, media queries, favicon

--- src/rootComponents/Header/Header.js
+++ src/rootComponents/Header/Header.js
@@ -4,6 +4,11 @@ import './Header.less'
 const Header = () => {
     return (
         <header className="header">
+            <span className='header__title'>
+                Przed wyruszeniem w drogę...
+                <br/>
+                sprawdź czy warto iść.
+            </span>
         </header>
     )
 }

f922e57352678c1fab0558ff7147a8f2752b1049
Wojciech Twaróg <wojciech.twarog@avra.pl>
2018-08-24 09:23:51 +0200

    footer upgrade

--- src/rootComponents/Footer/Footer.js
+++ src/rootComponents/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.less'
 const Footer = () => {
     return(
         <div className='footer'>
-            <span> W&W team in Avra </span>
+            <span> W&W in Avra </span>
             <div>
                 <a href="https://www.vecteezy.com">Illustrations by Vecteezy!</a>
             </div>
